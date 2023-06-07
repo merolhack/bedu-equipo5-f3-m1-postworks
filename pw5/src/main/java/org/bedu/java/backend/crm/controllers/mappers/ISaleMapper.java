02a1704d90459b6c1b2670f2bcced27470d55219
Lenin Jose Meza Zarco <merolhack [at] gmail.com>
2023-06-07 13:51:16 -0600

    :sparkles: feat(pw5): Mappers added

---
+++ pw5/src/main/java/org/bedu/java/backend/crm/controllers/mappers/ISaleMapper.java
@@ -0,0 +1,10 @@
+package org.bedu.java.backend.crm.controllers.mappers;
+
+import org.mapstruct.Mapper;
+
+/**
+ * @author Lenin Meza merolhack@gmail.com
+ */
+@Mapper
+public interface ISaleMapper {
+}
